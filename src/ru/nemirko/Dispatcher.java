829d57f8fe6074052f054fb44b1c0e4c8a88bb9d
anemirko <anemirko@gmail.com>
2018-02-18 10:11:52 +0300

    fix Dispatcher

--- src/ru/nemirko/Dispatcher.java
+++ src/ru/nemirko/Dispatcher.java
@@ -5,10 +5,11 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.*;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Random;
+import java.util.Set;
+import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -23,27 +24,42 @@ import java.util.stream.IntStream;
 public class Dispatcher {
 
     private AtomicInteger counter = new AtomicInteger(1);
-    private Path root;
     private int maxDrivers;
+    private BlockingQueue<Message> queue = new LinkedBlockingQueue<>();
     private Map<Integer,Future<LocalDateTime>> map = new ConcurrentHashMap<>();
     private Set<Driver> drivers;
+    private final ExecutorService service = Executors.newFixedThreadPool(1);
+    private volatile boolean stop = false;
     private static final Logger LOGGER = Logger.getLogger(Dispatcher.class.getName());
 
     public Dispatcher(Path root, int maxDrivers) {
-        this.root = root;
         this.maxDrivers = maxDrivers;
-
-        List<Path> driversPath = IntStream
+        drivers = IntStream
                 .rangeClosed(1, maxDrivers)
-                .mapToObj(i -> Paths.get(root.toString(), Integer.toString(i)))
-                .collect(Collectors.toList());
-
-        Set<Driver> set = new LinkedHashSet<>();
-        for (int i = 0; i < driversPath.size(); i++) {
-            Util.prepareDirectory(driversPath.get(i)); //Создать или очистить директорию
-            set.add(new Driver(i + 1, driversPath.get(i)));
-        }
-        drivers = Collections.unmodifiableSet(set);
+                .mapToObj(i -> {
+                    Path path = Paths.get(root.toString(), Integer.toString(i));
+                    Util.prepareDirectory(path); //Создать или очистить директорию
+                    return new Driver(i, path);
+                }).collect(Collectors.toSet());
+        service.execute(() -> {
+            try {
+                while (!stop) {
+                    Message message = queue.poll(100, TimeUnit.MILLISECONDS);
+                    if (message != null) {
+                        Integer targetId = message.getTarget().getId();
+                        Driver driver = getDriverById(targetId);
+                        if (driver != null) {
+                            map.put(message.getDispatched().getId(), driver.addMessage(message));
+                            LOGGER.info(message + " add in queue driver " + targetId);
+                        } else {
+                            LOGGER.log(Level.WARNING,message + " not found driver with id=" + targetId);
+                        }
+                    }
+                }
+            } catch (InterruptedException e) {
+                LOGGER.log(Level.WARNING, "Прервана работа диспетчера");
+            }
+        });
     }
 
     private Driver getDriverById(Integer id) {
@@ -55,36 +71,40 @@ public class Dispatcher {
         return null;
     }
 
-    public synchronized Integer processMessage(String xml) throws JAXBException {
+    public Integer processMessage(String xml) throws JAXBException {
         Message message = Util.messageFromXML(xml);
-        Integer targetId = message.getTarget().getId();
         message.setDispatched(counter.getAndIncrement());
-        map.put(message.getDispatched().getId(), getDriverById(targetId).addMessage(message));
-        LOGGER.info(message + " add in queue driver "+ targetId);
+        queue.add(message);
+        LOGGER.info(message + " add in dispatcher's queue");
         return message.getDispatched().getId();
     }
+
     public String getStatusMessageById(Integer dispatchedId) {
-        if (map.get(dispatchedId).isDone()) {
-            try {
-                return String.format("Заказ %d выполнен %s", dispatchedId,
-                        map.get(dispatchedId).get().format(DateTimeFormatter.ISO_DATE_TIME));
-            } catch (InterruptedException | ExecutionException e) {
-                LOGGER.log(Level.WARNING, e.getMessage());
+        if (map.containsKey(dispatchedId)) {
+            if (map.get(dispatchedId).isDone()) {
+                try {
+                    return String.format("Заказ %d выполнен %s", dispatchedId,
+                            map.get(dispatchedId).get().format(DateTimeFormatter.ISO_DATE_TIME));
+                } catch (InterruptedException | ExecutionException e) {
+                    LOGGER.log(Level.WARNING, e.getMessage());
+                }
             }
+            return String.format("Заказ %d пока не исполнен", dispatchedId);
         }
-        return String.format("Заказ %d пока не исполнен", dispatchedId);
+        return String.format("Заказ %d пока не обрабатывался", dispatchedId);
     }
 
     public void shutdown() {
-        this.drivers.forEach(Driver::shutdown);
+        stop = true;
+        service.shutdown();
+        drivers.forEach(Driver::shutdown);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException{
         Dispatcher dispatcher = new Dispatcher(Paths.get("/Users/anemirko/Documents/taxi/"), 10);
-        new Random().ints(25, 1, dispatcher.maxDrivers + 1)
+        new Random().ints(1000, 1, dispatcher.maxDrivers + 1)
                 .forEach(value -> {
                     Message message = Util.createMessage(value);
-                    System.out.println(message);
                     try {
                         int id = dispatcher.processMessage(Util.messageToXML(message));
                         LOGGER.info("Generate message dispatched id = " + id);
@@ -92,11 +112,11 @@ public class Dispatcher {
                         LOGGER.log(Level.WARNING, e.getMessage());
                     }
                 });
-        try {
-            Thread.sleep(8000);
-        } catch (Exception e) {}
+
+        //Проверка получения статуса выполнения заказа
+        TimeUnit.SECONDS.sleep(8);
         System.out.println(dispatcher.getStatusMessageById(1));
-        System.out.println(dispatcher.getStatusMessageById(23));
+        System.out.println(dispatcher.getStatusMessageById(546));
         dispatcher.shutdown();
     }
 }
